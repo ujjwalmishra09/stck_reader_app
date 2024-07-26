a97cc4e466f1b0b5900367cc09ca6a5feac042bd
Ujjwal Mishra <ujjwalmishra09@gmail.com>
2024-07-26 13:35:27 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'stck_app_reader',
+  webDir: 'dist'
+};
+
+export default config;
